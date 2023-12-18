6cada55c157d4d4a12cf9115afe5b9b439d283d2
Jan Janßen <janssen@mpie.de>
2023-12-18 17:11:25 +0100

    python example

--- resources/quantumespresso/bin/run_quantumespresso_6.3_mpi.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-# export ESPRESSO_PSEUDO=${DIR}/../potentials
-export ESPRESSO_PSEUDO=${CONDA_PREFIX}/share/pslibrary/pbe/PSEUDOPOTENTIALS/
-mpirun -np ${1} pw.x -in pyiron.pwi > pyiron.pwo
